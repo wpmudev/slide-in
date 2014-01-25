3b9162e495c395aa440f0b5233d0f98b905d0789
vladislav <vladislav@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-01-25 13:33:41 +0000

    Updating the authorship information.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/slide-in@12640 f53fe46e-8c02-459b-9fdd-93e75771b278

--- loader.php
+++ loader.php
@@ -10,7 +10,7 @@ Author URI: http://premium.wpmudev.org
 WDP ID: 694503
 
 Copyright 2009-2013 Incsub (http://incsub.com) 
-Authors: Jeffri Hong (Incsub), Victor Ivanov (Incsub), Ve Bailovity (Incsub)
+Authors - Jeffri Hong (Incsub), Victor Ivanov (Incsub), Ve Bailovity (Incsub)
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
 the Free Software Foundation.
